1e5ec0bb7837305fc99ebcf0fe4c9d0103887d14
tolbertinnovation-debug <tolbertinnovation@gmail.com>
2026-08-18 00:57:23 -0700

    Bump marketing-video-map cache to v=3

--- topic-videos.js
+++ topic-videos.js
@@ -7,7 +7,7 @@ document.write('<script src="complit-video-map.js?v=1"><\/script>');
 document.write('<script src="aicyber-video-map.js?v=1"><\/script>');
 document.write('<script src="entrepreneurship-video-map.js?v=1"><\/script>');
 document.write('<script src="android-video-map.js?v=2"><\/script>');
-document.write('<script src="marketing-video-map.js?v=2"><\/script>');
+document.write('<script src="marketing-video-map.js?v=3"><\/script>');
 document.write('<script src="projectmgmt-video-map.js?v=1"><\/script>');
 document.write('<script src="leadership-video-map.js?v=1"><\/script>');
 document.write('<script src="complit-notes-1.js?v=3"><\/script>');
